9937eacd0ae3c145ace0492c46349a456e24cae7
Subash <it.subashaa@gmail.com>
2022-06-15 22:49:59 +0530

    TimerRing and changed time format (mins:secs)

--- PomodoroTimer/Assets/Scripts/Pomodoro.cs
+++ PomodoroTimer/Assets/Scripts/Pomodoro.cs
@@ -4,15 +4,19 @@ using UnityEngine.UI;
 public class Pomodoro : MonoBehaviour
 {
     [SerializeField] Text TimerText;
+    [SerializeField] Image TimeSlider;
     [SerializeField] float setTimer = 25f;
 
     private float currentTime;
+    private float maxTimeInMins;
     public bool startTimer = false;
 
     private void Start()
     {
+        maxTimeInMins = setTimer * 60f;
         currentTime = setTimer * 60f;
         startTimer = false;
+        TimeSlider.fillAmount = currentTime/ maxTimeInMins;
     }
 
     private void Update()
@@ -23,7 +27,9 @@ public class Pomodoro : MonoBehaviour
 
     void SetTimerText()
     {
-        TimerText.text = currentTime.ToString("0");
+        float mins = Mathf.Floor(currentTime / 60f);
+        float secs = Mathf.Floor(currentTime % 60f);
+        TimerText.text = mins.ToString("00") +":"+ secs.ToString("00");
     }
 
     public void Timer()
@@ -32,6 +38,7 @@ public class Pomodoro : MonoBehaviour
         {
             currentTime -= 1 * Time.deltaTime;
         }
+        TimeSlider.fillAmount = Mathf.Lerp(TimeSlider.fillAmount, currentTime / maxTimeInMins, 10 * Time.deltaTime);
     }
     public void StartTimer()
     {
