efefb9497f672fc2f3cced2a0c9c47804958ef2b
Subash <it.subashaa@gmail.com>
2022-06-19 17:11:44 +0530

    Datastructure for storing taskId
    used List<int> to store hashId of TaskNames

--- PomodoroTimer/Assets/Scripts/Task.cs
+++ PomodoroTimer/Assets/Scripts/Task.cs
@@ -4,13 +4,25 @@ using UnityEngine.UI;
 public class Task : MonoBehaviour
 {
     [SerializeField] Text TaskName;
-    public void SetTask(string taskName)
+    public int taskId;
+
+    private TodoList todoList;
+
+    private void Start()
+    {
+        todoList = FindObjectOfType<TodoList>();
+    }
+
+    public void SetTask(string taskName, int id)
     {
         TaskName.text = taskName;
+        taskId = id;
+        Debug.Log("Task Created with ID: " + taskId);
     }
 
     public void DeleteTask()
     {
+        todoList.Tasks.Remove(taskId);
         Debug.Log("Task Removed");
         Destroy(gameObject);       
     }
--- PomodoroTimer/Assets/Scripts/TodoList.cs
+++ PomodoroTimer/Assets/Scripts/TodoList.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class TodoList : MonoBehaviour
@@ -6,17 +7,32 @@ public class TodoList : MonoBehaviour
     [SerializeField] GameObject Task;
     [SerializeField] InputField TaskInput;
     [SerializeField] Transform Content;
-    
+
+    public List<int> Tasks = new List<int>();
+
     public void AddTask()
     {
         string taskText = TaskInput.text;
+
         if (string.IsNullOrEmpty(taskText))
         {
-            taskText = "Empty Task";
+            Debug.Log("Enter a Valid Task name");
+            return;
         }
+
+        string taskName = taskText;
+        int taskId = taskText.GetHashCode();
+
+        if (Tasks.Contains(taskId))
+        {
+            Debug.Log("Task is already in Todo List!");
+            return;
+        }
+
         GameObject task = Instantiate(Task, Content);
         Task currentTask = task.GetComponent<Task>();
-        currentTask.SetTask(taskText);
-    }
+        currentTask.SetTask(taskName, taskId);
 
+        Tasks.Add(taskId);
+    }
 }
